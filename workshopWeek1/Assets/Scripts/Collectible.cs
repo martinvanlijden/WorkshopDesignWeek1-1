932988c85c0ad9d52de7f4eddc7e045ae51c5ff6
BramSoutendam <49754071+BramSoutendam@users.noreply.github.com>
2025-09-11 11:12:36 +0200

    basic collectible made

---
+++ workshopWeek1/Assets/Scripts/Collectible.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collectible entered by:" + other.tag);
+        if (other.tag == "Player")
+        {
+            Debug.Log("collected!!");
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
