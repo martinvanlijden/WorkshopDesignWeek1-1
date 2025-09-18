aa9226f87884306453b1567fcbb9764192509c13
BramSoutendam <49754071+BramSoutendam@users.noreply.github.com>
2025-09-18 11:52:12 +0200

    Simple Cam finalized

--- workshopWeek2/WorkshopWeek2/Assets/Scripts/Camera.cs
+++ workshopWeek2/WorkshopWeek2/Assets/Scripts/Camera.cs
@@ -1,19 +1,25 @@
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
+public class CameraMovement : MonoBehaviour
+{
+    
+    public GameObject player;
 
-    public Transform target;
-
-    public float smoothSpeed = 1f;
-    public Vector3 offset;
-
-    void FixedUpdate ()
+    private Vector3 _offset;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
-
-        transform.LookAt(target);
+        // Calculate difference between camera and player
+        // Only need to do this once
+        _offset = transform.position - player.transform.position;
     }
 
+    // Runs after all the other update functions have been called
+    // Good for camera movement
+    private void LateUpdate()
+    {
+        // Each frame, set the camera's position to the player's position plus the offset
+        transform.position = player.transform.position + _offset;
+    }
 }
